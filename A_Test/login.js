b9989139621d3f5f6c89e8f4f0e160d197c857b4
Vu Oppa <you@example.com>
2020-11-05 17:15:05 +0700

    login register

---
+++ A_Test/login.js
@@ -0,0 +1,27 @@
+function validationForm() {
+  //validation name
+  var fullName = $("#fullName").val();
+  if (fullName == '') {
+    $("#errorFullName").text("Please enter full name").show();
+    return false;
+  } else {
+    var patternFullName = /^[a-zA-Z ]$/
+    if (!patternFullName.test(fullName)) {
+      $("#errorFullName").text("Please enter right validation").show();
+      return false;
+    }
+  }
+  //validation identification
+  var fullName = $("#fullName").val();
+  if (fullName == '') {
+    $("#errorFullName").text("Please enter full name").show();
+    return false;
+  } else {
+    var patternFullName = /^\d+$/
+    if (!patternFullName.test(fullName)) {
+      $("#errorFullName").text("Please enter right validation").show();
+      return false;
+    }
+  }
+  return true;
+}
